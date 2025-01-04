207bc51cb9f4771b8196663dffce6b5746995009
sameer2748 <mrao27488@gmail.com>
2025-01-04 23:25:47 +0530

    afv jsbv

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const app = express();
 app.get("/", (req, res) => {
   res.json({ success: "aws testing done by sameer" });
 });
+app.get("/Nancy", (req, res) => {
+  res.json({ success: "Love you nancy" });
+});
 
 app.listen(3000, () => {
   console.log("started on prit 3000");
