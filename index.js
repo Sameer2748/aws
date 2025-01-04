2d6af0c67b67001202319edfa473bb91a6545852
sameer2748 <mrao27488@gmail.com>
2025-01-04 16:12:16 +0530

    added first aws commit

---
+++ index.js
@@ -0,0 +1,11 @@
+import express from "express";
+
+const app = express();
+
+app.get("/sameer", (req, res) => {
+  res.json({ success: "aws testing done by sameer" });
+});
+
+app.listen(3000, () => {
+  console.log("started on prit 3000");
+});
